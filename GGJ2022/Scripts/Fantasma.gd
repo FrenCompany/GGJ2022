02f411b61cd42094d6846d9fd6b79f9f93a6c58c
tomimi <tomyahu@gmail.com>
2022-01-30 15:39:22 -0300

    direccion del fantasma se setea al inicio

--- GGJ2022/Scripts/Fantasma.gd
+++ GGJ2022/Scripts/Fantasma.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
-export var dir = Vector2(0,0)
+export var dir = Vector2(1,0)
 export var vel_max = 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	dir = Vector2(randi()%3-1,randi()%3-1).normalized() * vel_max
+	dir = dir.normalized() * vel_max
 	if dir == Vector2(0,0):
 		dir = Vector2(0, vel_max)
 
